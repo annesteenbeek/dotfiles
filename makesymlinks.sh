b58538beffef34383b2a7840ffe44dc6c4b4d73c
annesteenbeek <annesteenbeek@gmail.com>
2016-04-11 17:26:46 +0200

    functioning

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -13,23 +13,26 @@ packages="zsh tmux source-highlight vim"             # packages to be installed
 
 ##########
 
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
+place_dotfiles() {
+    # create dotfiles_old in homedir
+    echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+    mkdir -p $olddir
+    echo "done"
 
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
+    # change to the dotfiles directory
+    echo -n "Changing to the $dir directory ..."
+    cd $dir
+    echo "done"
+
+    # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+    for file in $files; do
+        echo "Moving any existing dotfiles from ~ to $olddir"
+        mv ~/.$file ~/dotfiles_old/
+        echo "Creating symlink to $file in home directory."
+        ln -s $dir/$file ~/.$file
+    done
+}
 
 install_packages () {
     for package in $packages; do
@@ -54,3 +57,4 @@ gsettings set com.canonical.Unity.Lenses disabled-scopes "['more_suggestions-ama
 install_packages
 install_omzsh
 disable_ubuntu_crap
+place_dotfiles
